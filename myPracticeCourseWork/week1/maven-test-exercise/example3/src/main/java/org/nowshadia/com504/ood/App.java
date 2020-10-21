09a9f4143151deb5e4360a588b7e5eedafd051db
amy-now <55843493+Amy-now@users.noreply.github.com>
2020-10-21 09:02:56 +0100

    week 2

---
+++ myPracticeCourseWork/week1/maven-test-exercise/example3/src/main/java/org/nowshadia/com504/ood/App.java
@@ -0,0 +1,13 @@
+package org.nowshadia.com504.ood;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ myPracticeCourseWork/week1/maven-test-exercise/example3/src/test/java/org/nowshadia/com504/ood/MyTestClassTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.nowshadia.com504.ood;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author Amy
+ */
+public class MyTestClassTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        MyTestClassLog4j myTest = new MyTestClassLog4j();
+        myTest.writeAboutMe();
+    }
+}
+
+
